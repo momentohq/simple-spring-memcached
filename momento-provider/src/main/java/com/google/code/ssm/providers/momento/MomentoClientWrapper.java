9aa5115959c3a91ccc3fae6f0d527f6872e940bd
Nate Anderson <nate@momentohq.com>
2025-03-05 09:57:37 -0800

    fix: correct debug logging format

--- momento-provider/src/main/java/com/google/code/ssm/providers/momento/MomentoClientWrapper.java
+++ momento-provider/src/main/java/com/google/code/ssm/providers/momento/MomentoClientWrapper.java
@@ -78,7 +78,7 @@ class MomentoClientWrapper extends AbstractMemcacheClientWrapper {
     public boolean delete(final String key) throws CacheException {
         try {
             momentoClient.delete(defaultCacheName, key);
-            LOGGER.debug(MessageFormat.format("Delete: Success Item removed from cache key: {1}", key));
+            LOGGER.debug(MessageFormat.format("Delete: Success Item removed from cache key: {0}", key));
             return true;
         } catch (RuntimeException e) {
             throw new CacheException(e);
